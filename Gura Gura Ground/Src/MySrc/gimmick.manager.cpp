93503d7a9d2b4beeec9c0c4396b989d97c465612
gotouyuuki0208 <231103@st.yoshida-g.ac.jp>
2025-11-12 14:29:57 +0900

    生成と削除ができた。ぐちゃぐちゃのソースコード

--- Gura Gura Ground/Src/MySrc/gimmick.manager.cpp
+++ Gura Gura Ground/Src/MySrc/gimmick.manager.cpp
@@ -15,7 +15,8 @@
 // デフォルトコンストラクタ
 //============================================================================
 CGimmickManager::CGimmickManager():
-	m_SpawnTime(0)
+	m_SpawnTime(0),
+	m_WarningActive(false)
 {}
 
 //============================================================================
@@ -29,20 +30,37 @@ CGimmickManager::~CGimmickManager()
 //============================================================================
 bool CGimmickManager::Initialize()
 {
-	//ギミック発生条件の設定
-	SpawnCondition Condition;
+	// ギミック出現時間
+	constexpr int SPAWN_TIME = 300;
 
-	//生成条件
-	Condition.condition = [this]() -> bool
-	{
-		return m_SpawnTime > 120;
-	};
+	// ギミック発生条件の設定
+	SpawnCondition SCondition;
 
-	//生成数
-	Condition.quantity = 1;
+	// 条件
+	SCondition.trigger = "time";
 
-	//保存
-	m_SpawnConditions.push_back(Condition);
+	// 生成する経過時間
+	SCondition.triggerTime = SPAWN_TIME;
+	
+	// 生成判定
+	SCondition.IsSpawned = false;
+
+	// ギミック削除条件の設定
+	DeleteCondition DCondition;
+	
+	// 条件
+	DCondition.trigger = "time";
+
+	// 削除する経過時間
+	DCondition.triggerTime = SPAWN_TIME;
+
+	// 削除判定
+	DCondition.IsDelete = false;
+
+	//ギミック関する情報を設定
+	m_GimmickEntry.DeleteCondition = DCondition;	// 削除条件を設定
+	m_GimmickEntry.SpawnConditions = SCondition;	// 生成条件を設定
+	m_GimmickEntry.Gimmick = nullptr;				// 生成しない
 
 	return true;
 }
@@ -60,22 +78,26 @@ void CGimmickManager::Finalize()
 //============================================================================
 void CGimmickManager::Update()
 {
+	m_WarningActive = false;
 	m_SpawnTime++;
 
 	// ギミックを出現させるか判定
 	CanSpawnGimmick();
+
+	// ギミックを消す
+	GimmickDelete();
 }
 
 //============================================================================
 // 生成処理
 //============================================================================
-void  CGimmickManager::Create(int Num)
+void  CGimmickManager::Create()
 {
-	for (int i = 0; i < Num; i++)
-	{
-		//鉄球の生成
-		CObject::Create<CIronBall>(OBJ::TYPE::NONE, OBJ::LAYER::DEFAULT, CIronBall::s_fpDefaultFactory);
-	}
+	// 鉄球の生成
+	auto Gimmick = CObject::Create<CIronBall>(OBJ::TYPE::NONE, OBJ::LAYER::DEFAULT, CIronBall::s_fpDefaultFactory);
+	m_GimmickEntry.Gimmick = Gimmick;
+	m_GimmickEntry.SpawnConditions.IsSpawned = true;
+
 }
 
 //============================================================================
@@ -83,17 +105,79 @@ void  CGimmickManager::Create(int Num)
 //============================================================================
 void CGimmickManager::CanSpawnGimmick()
 {
-	for (auto it = m_SpawnConditions.begin(); it != m_SpawnConditions.end();)
-	{
-		if (it->condition())
-		{
-			Create(it->quantity);
+	// ギミック出現の〇秒前に警告を出す時間
+	constexpr int WARNING_FRAME = 120;
 
-			it = m_SpawnConditions.erase(it);
+	//for (auto it = m_SpawnConditions.begin(); it != m_SpawnConditions.end();)
+	//{
+		if (CanSpawnCondition(m_GimmickEntry.SpawnConditions))
+		{//条件を満たしてるとき
+
+			// 生成
+			Create();
+
+			//it = m_SpawnConditions.erase(it);
 		}
 		else
 		{
-			it++;
+			//出現時間
+			int TriggerTime = m_GimmickEntry.SpawnConditions.triggerTime;
+
+			if (!m_WarningActive								 // 警告中ではない
+				&& m_SpawnTime >= (TriggerTime - WARNING_FRAME)) // かつ出現時間の2秒前
+			{
+				m_WarningActive = true;
+			}
+
+			//it++;
+		}
+	//}
+}
+
+//============================================================================
+// 出現条件を満たしてるか
+//============================================================================
+bool CGimmickManager::CanSpawnCondition(const SpawnCondition& Condition)
+{
+	if (Condition.trigger == "time")
+	{//時間経過が条件のとき
+
+		if (m_SpawnTime >= Condition.triggerTime
+			&& !Condition.IsSpawned)
+		{
+			return true;
 		}
 	}
+
+	return false;
+}
+
+//============================================================================
+// ギミックを消す
+//============================================================================
+void CGimmickManager::GimmickDelete()
+{
+	if (m_GimmickEntry.DeleteCondition.trigger == "time")
+	{//時間経過が条件のとき
+
+		if (m_GimmickEntry.DeleteCondition.IsDelete)
+		{
+			return;
+		}
+
+		// 生成時間
+		int SpawnTriggerTime = m_GimmickEntry.SpawnConditions.triggerTime;
+
+		// 消去時間
+		int DeleteTriggerTime = m_GimmickEntry.DeleteCondition.triggerTime;
+
+		if (m_SpawnTime >= SpawnTriggerTime + DeleteTriggerTime)
+		{//消去するまでの時間が経過した
+
+			m_GimmickEntry.Gimmick->SetDeath();
+			m_GimmickEntry.DeleteCondition.IsDelete = true;
+			m_GimmickEntry.Gimmick = nullptr;
+		}
+	}
+
 }
--- Gura Gura Ground/Src/MySrc/gimmick.manager.h
+++ Gura Gura Ground/Src/MySrc/gimmick.manager.h
@@ -11,6 +11,12 @@
 // インクルードファイル
 //****************************************************
 #include "API.singleton.h"
+#include "API.object.h"
+
+//****************************************************
+// 前方宣言
+//****************************************************
+class CIronBall;
 
 //****************************************************
 // ギミックマネージャークラスの定義
@@ -23,11 +29,33 @@ class CGimmickManager final : public CSingleton<CGimmickManager>
 	//****************************************************
 	struct SpawnCondition
 	{
-		//float triggerTime;               // 経過時間で出現
+		int triggerTime;				 // 経過時間で出現
+		std::string trigger;			 // 条件
 		std::function<bool()> condition; // 出現条件
+		bool IsSpawned;					 // 生成されたか
 		//DirectX::XMFLOAT3 spawnPosition; // 出現位置
 		//std::string objectType;			 // 何を出すか
-		int quantity;                    // 数量
+	};
+
+	//****************************************************
+	// 消去条件を表す構造体
+	//****************************************************
+	struct DeleteCondition
+	{
+		int triggerTime;               // 経過時間で消去
+		std::string trigger;			 // 条件
+		std::function<bool()> condition; // 消去条件
+		bool IsDelete;					 // 消去されたか
+	};
+
+	//****************************************************
+	// ギミックの管理をする構造体
+	//****************************************************
+	struct GimmickEntry
+	{
+		CIronBall* Gimmick;					// ギミックのポインタ
+		SpawnCondition SpawnConditions;		// 出現条件
+		DeleteCondition DeleteCondition;	// 消去条件
 	};
 
 	//****************************************************
@@ -44,7 +72,9 @@ public:
 
 	// 更新処理
 	void Update();
-
+	
+	// 警告中かの判定を取得
+	bool GetWarningActive()const { return m_WarningActive; }
 private:
 
 	//****************************************************
@@ -64,15 +94,23 @@ private:
 	void Finalize();
 
 	// 生成
-	void Create(int Num);
+	void Create();
 
 	// ギミックを出現させるか判定
 	void CanSpawnGimmick();
 
+	// 出現条件を満たしてるか
+	bool CanSpawnCondition(const SpawnCondition& Condition);
+
+	// ギミックを消す
+	void GimmickDelete();
+
 	//****************************************************
 	// data
 	//****************************************************
-
-	std::vector<SpawnCondition> m_SpawnConditions;
-	int m_SpawnTime;
+	std::list<CObject*> m_GimmickList;				// ギミックを格納
+	std::vector<SpawnCondition> m_SpawnConditions;	// 出現条件を格納
+	GimmickEntry m_GimmickEntry;					// ギミックの管理
+	int m_SpawnTime;								// 時間経過カウント
+	bool m_WarningActive;							// 警告表示中か
 };
