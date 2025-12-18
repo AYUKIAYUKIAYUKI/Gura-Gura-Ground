8b64fdf66de18eefb87435223aa3dc4936503990
AYUKIAYUKIAYUKI <231018@st.yoshida-g.ac.jp>
2025-12-18 14:44:23 +0900

    ライブラリと公開APIの更新 ⇒ オブジェクトを標準ライブラリのクラスを継承しています + プレイヤー、ボム、衝撃波を変更しています

--- Gura Gura Ground/Src/MySrc/player.h
+++ Gura Gura Ground/Src/MySrc/player.h
@@ -15,7 +15,7 @@
 //****************************************************
 // 前方宣言
 //****************************************************
-class CShockWave;
+class CField;
 
 //****************************************************
 // プレイヤークラスの定義
@@ -25,6 +25,8 @@ class CPlayer : public CPhysicsObject
 	//****************************************************
 	// 静的定数
 	//****************************************************
+
+	// 塵生成の最大ステップ数
 	static constexpr int DUST_STEP_COUNT_MAX = 15;
 
 public:
@@ -47,36 +49,38 @@ public:
 	// コライダーのファクトリ
 	void FactoryCollider(float fWidth = 1.0f, float fHeight = 1.0f, float fDepth = 1.0f) override;
 
-	// 衝撃波の作成
-	void CreateShockWave(Collision::SHAPETYPE Type, const DirectX::XMFLOAT3A& Size, int nDuration);
-
-	// 衝撃波の削除
-	void DeleteShockWave();
-
 	// 更新処理
 	void Update() override;
 
-	// 塵の進行更新
-	void UpdateDustStep(const DirectX::XMFLOAT3& Direction);
-
 	// 描画処理
 	void Draw() override;
+
+	// 衝撃波の作成
+	void CreateShockWave(Collision::SHAPETYPE Type, const DirectX::XMFLOAT3A& Size, int nDuration);
 
 	// プレイヤーのインデックス操作用
 	unsigned char GetIdxPlayer() const;
 	void          SetIdxPlayer(unsigned char wIdx);
 
-	// 制御不能期間の操作用
+	// 操作不能期間の操作用
 	int  GetLostControlDuration() const;
 	void SetLostControlDuration(int nTime);
 
+	// 塵の進行更新
+	void UpdateDustStep(const DirectX::XMFLOAT3& Direction);
+
 private:
+
+	//****************************************************
+	// function
+	//****************************************************
+	void CheckDeath(); // 死亡チェック
 
 	//****************************************************
 	// data
 	//****************************************************
 	std::unique_ptr<StateMachine> m_upStateMachine;       // 状態管理
-	std::weak_ptr<CShockWave>     m_wpShockWave;          // 衝撃波
+	std::weak_ptr<CField>         m_wpField;              // 地面
 	unsigned char                 m_wIdxPlayer;           // プレイヤーのインデックス
 	int                           m_nLostControlDuration; // 操作不能期間
 	int                           m_nStepCounter;         // 進行カウンター
