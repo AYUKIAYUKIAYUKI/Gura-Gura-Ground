8ca150da98c95c120bcd439467d36991d8cfddf3
AYUKIAYUKIAYUKI <231018@st.yoshida-g.ac.jp>
2025-12-23 10:32:58 +0900

    ゲームシーンを整理しました ・多少のグローバルや局所変数の改善 ・生成処理周りの構造化

--- Gura Gura Ground/Src/MySrc/scene.game.cpp
+++ Gura Gura Ground/Src/MySrc/scene.game.cpp
@@ -27,12 +27,11 @@
 #include "cameracontroller.h"
 
 //****************************************************
-// 仮
+// 仮：最終的に必要と判断した変数はメンバに付属してください
 //****************************************************
 namespace
 {
 	// 定数
-	const int nNumB = 4;
 	const float fInitDist = 10.0f;
 
 	// オブジェクトの出現方向, 0:縦(上下), 1:横(左右)
@@ -48,24 +47,6 @@ namespace
 
 	std::chrono::steady_clock::time_point g_LastUpdateTime;
 	float g_GameTime = 0.0f;
-
-	// グローバル
-	OBJ::Transform g_BoxTF = { { 0.5f, 0.5f, 0.5f }, {0.0f, 0.0f, 0.0f, 1.0f}, {-fInitDist, 25.0f, -fInitDist} };
-
-	/* シンプルなゲームセット */
-	bool GameSet()
-	{
-		// プレイヤーのリストを取得
-		const auto& rPlayerList = CObjectManager::RefInstance().RefListShare(OBJ::TYPE::PLAYER);
-
-		// 一体もプレイヤーが存在しないなら (本当はそうでは無い)
-		if (rPlayerList.size() < 1)
-		{
-			return true;
-		}
-
-		return false;
-	}
 }
 
 //============================================================================
@@ -73,77 +54,25 @@ namespace
 //============================================================================
 CSceneGame::CSceneGame()
 {
-	// コリジョン描画の切り替え
+	/* コリジョン描画の切り替え */
 	CCollider::SwitchRenderCollision();
 
-	// 初期設定
+	// カメラコントローラーの初期化
 	CCameraController::RefInstance().Initialize();
 
-	// 地面を生成
-	float fSpanField = 15.0f;
-	CObjectManager::CreateShare<CField>(
-		[&fSpanField](CField* p) -> bool
-		{
-			p->SetTransform(
-				{
-					{ fSpanField, 1.0f, fSpanField },
-					{ 0.0f, 0.0f, 0.0f, 1.0f },
-					{ 0.0f, 5.0f, 0.0f }
-				}
-			);
+	// フィールドスポーン
+	SpawnField();
 
-			p->FactoryCollider(fSpanField * 2.0f, 1.0f * 2.0f, fSpanField * 2.0f);
-			return true;
-		},
-		OBJ::TYPE::FIELD);
+	// プレイヤースポーン
+	SpawnPlayer();
 
-	// ああ
-	const float fUnkoSpan = 3.0f;
+	// CPUスポーン
+	SpawnCPU();
 
-	// プレイヤーの生成
-	for (unsigned char i = 0; i < nNumB; ++i)
-	{
-		// 良い感じに四方に散らばらせる
-		if (i % 2 == 0) g_BoxTF.Pos.z *= -1.0f;
-		if (i % 2 == 1) g_BoxTF.Pos.x *= -1.0f;
-
-		auto spPlayer = CObjectManager::CreateShare<CPlayer>(
-			[&i](CPlayer* p) -> bool
-			{
-				p->SetIdxPlayer(i);
-				p->SetTransform(g_BoxTF);
-				p->FactoryCollider(1.0f, 1.0f, 1.0f);
-				return true;
-			},
-			OBJ::TYPE::PLAYER);
-
-		// プレイヤー登録
-		CCameraController::RefInstance().Regist(spPlayer.get());
-	}
-
+	// 障害物エディターの初期化
 	m_ObstacleEditer.LoadParams("Data\\JSON\\obscale_table.json"); //障害物パラメーターを読み込む
 	g_LastUpdateTime = std::chrono::steady_clock::now(); //現在の時間に合わせる
 	g_GameTime = 0.0f;
-
-
-	// 敵生成
-	float fSize = 1.0f;
-
-	CObjectManager::CreateRaw<CEnemy1>(
-		[&fSize](CEnemy1* p) -> bool
-		{
-			p->SetTransform(
-				{
-					{ fSize, fSize, fSize },
-					{ 0.0f, 0.0f, 0.0f, 1.0f },
-					{ 2.0f, 15.0f, 2.0f }
-				}
-			);
-
-			p->FactoryCollider(fSize, fSize, fSize);
-			return true;
-		},
-		OBJ::TYPE::NONE); //TYPEはENEMYとか別枠で確保した}
 }
 
 //============================================================================
@@ -172,11 +101,13 @@ void CSceneGame::Update()
 	//プレイモード中の自動スポーン処理
 	m_ObstacleEditer.PlayModeSpawn(deltaTime);
 	CCameraController::RefInstance().Update();	// ゲームセットしたらシーン遷移
-	if (GameSet())
+	
+	/* ゲームセットチェック */
+	if (CheckGameSet())
 	{
+		/* 即シーン変更 */
 		Change();
 	}
-
 }
 
 //============================================================================
@@ -189,4 +120,122 @@ void CSceneGame::Change()
 
 	// タイトルシーンへ
 	CSceneManager::RefInstance().ChangeScene(std::make_unique<CSceneTitle>());
+}
+
+//============================================================================
+// フィールドスポーン
+//============================================================================
+void CSceneGame::SpawnField()
+{
+	// フィールドの水平方向の大きさ
+	const float fSpanField = 15.0f;
+	
+	// 地面を生成
+	CObjectManager::CreateShare<CField>(
+		[&fSpanField](CField* p) -> bool
+		{
+			// トランスフォームの設定
+			p->SetTransform(
+				{
+					{ fSpanField, 1.0f, fSpanField },
+					{ 0.0f, 0.0f, 0.0f, 1.0f },
+					{ 0.0f, 5.0f, 0.0f }
+				});
+
+			// コライダーの生成
+			p->FactoryCollider(fSpanField * 2.0f, 1.0f * 2.0f, fSpanField * 2.0f);
+
+			return true;
+		},
+		OBJ::TYPE::FIELD);
+}
+
+//============================================================================
+// プレイヤースポーン
+//============================================================================
+void CSceneGame::SpawnPlayer()
+{
+	// プレイヤーの初期トランスフォーム
+	OBJ::Transform PlayersInitTransform = 
+	{
+		{ 0.5f, 0.5f, 0.5f },
+		{ 0.0f, 0.0f, 0.0f, 1.0f},
+		{ -fInitDist, 25.0f, -fInitDist }
+	};
+
+	for (unsigned char wPlayerIndex = 0; wPlayerIndex < MAX_PLYAER; ++wPlayerIndex)
+	{
+		// 良い感じに四方に散らばらせる
+		if (wPlayerIndex % 2 == 0) PlayersInitTransform.Pos.z *= -1.0f;
+		if (wPlayerIndex % 2 == 1) PlayersInitTransform.Pos.x *= -1.0f;
+
+		// プレイヤー生成
+		const std::shared_ptr<CPlayer>& spPlayer = CObjectManager::CreateShare<CPlayer>(
+			[&PlayersInitTransform, wPlayerIndex](CPlayer* p) -> bool
+			{
+				// プレイヤーインデックスを決定
+				p->SetIdxPlayer(wPlayerIndex);
+
+				// トランスフォームの設定
+				p->SetTransform(PlayersInitTransform);
+
+				// コライダー生の成
+				p->FactoryCollider(1.0f, 1.0f, 1.0f);
+
+				return true;
+			},
+			OBJ::TYPE::PLAYER);
+
+		// プレイヤーの弱参照を作成
+		m_apwPlayers[wPlayerIndex] = spPlayer;
+
+		// カメラコントローラーへプレイヤーを登録
+		/* 出来れば生ポインタでなく弱参照を用いてください */
+		CCameraController::RefInstance().Regist(spPlayer.get());
+	}
+}
+
+//============================================================================
+// CPUスポーン
+//============================================================================
+void CSceneGame::SpawnCPU()
+{
+	// 敵の各辺のスパン
+	const float fSize = 1.0f;
+
+	// 敵生成
+	CObjectManager::CreateRaw<CEnemy1>(
+		[fSize](CEnemy1* p) -> bool
+		{
+			// トランスフォームの設定
+			p->SetTransform(
+				{
+					{ fSize, fSize, fSize },
+					{ 0.0f,  0.0f,  0.0f, 1.0f },
+					{ 2.0f,  15.0f, 2.0f }
+				});
+
+			// コライダーの生成
+			p->FactoryCollider(fSize, fSize, fSize);
+
+			return true;
+		},
+		OBJ::TYPE::NONE); //TYPEはENEMYとか別枠で確保した}
+}
+
+//============================================================================
+// ゲームセットチェック
+//============================================================================
+bool CSceneGame::CheckGameSet()
+{
+	for (const auto& wpPlayer : m_apwPlayers)
+	{
+		/* プレイヤーが1人でも生きていたらゲーム継続 */
+		if (!wpPlayer.expired())
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
